d32309fa26c8f04b238aabda132f32ac406611b9
codegangsk <kjhj0604@gmail.com>
2020-10-08 15:15:19 +0900

    show/hide date picker when dobLabel is tapped

--- EmployeeRoster/EmployeeDetailTableViewController.swift
+++ EmployeeRoster/EmployeeDetailTableViewController.swift
@@ -9,16 +9,32 @@ class EmployeeDetailTableViewController: UITableViewController, UITextFieldDeleg
     
     @IBOutlet var nameTextField: UITextField!
     @IBOutlet var dobLabel: UILabel!
+    @IBOutlet var dobDatePicker: UIDatePicker!
+    
     @IBOutlet var employeeTypeLabel: UILabel!
     
     var employee: Employee?
     
+    var isEditingBirthday: Bool = false {
+        didSet {
+            tableView.beginUpdates()
+            tableView.endUpdates()
+        }
+    }
+    let birthDateLabelIndex = IndexPath(row: 1, section: 0)
+    let birthDatePickerIndex = IndexPath(row: 2, section: 0)
+    
+}
+
+extension EmployeeDetailTableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
         updateView()
     }
-    
+}
+
+extension EmployeeDetailTableViewController {
     func updateView() {
         if let employee = employee {
             navigationItem.title = employee.name
@@ -33,7 +49,9 @@ class EmployeeDetailTableViewController: UITableViewController, UITextFieldDeleg
             navigationItem.title = "New Employee"
         }
     }
-    
+}
+
+extension EmployeeDetailTableViewController {
     @IBAction func saveButtonTapped(_ sender: Any) {
         if let name = nameTextField.text {
             employee = Employee(name: name, dateOfBirth: Date(), employeeType: .exempt)
@@ -45,11 +63,31 @@ class EmployeeDetailTableViewController: UITableViewController, UITextFieldDeleg
         employee = nil
         performSegue(withIdentifier: PropertyKeys.unwindToListIndentifier, sender: self)
     }
-    
-    // MARK: - Text Field Delegate
-    
+}
+
+extension EmployeeDetailTableViewController {
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         return false
     }
-
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath {
+        case birthDatePickerIndex:
+            if isEditingBirthday == true {
+                return dobDatePicker.frame.height
+            } else  {
+                return 0
+            }
+        default:
+                return UITableView.automaticDimension
+            }
 }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        if indexPath == birthDateLabelIndex {
+            isEditingBirthday = !isEditingBirthday
+        }
+    }
+}
+
